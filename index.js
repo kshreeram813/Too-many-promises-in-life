ad21778aa2306b5cc06eefb46084f086e98ec9e7
SHREERAM KUMAR KUSHWAHA <117209741+kshreeram813@users.noreply.github.com>
2023-04-06 20:46:40 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>practice promise</title>
+</head>
+<body>
+
+  <script>
+    const posts = [
+      { title: 'Post One', body: 'This is post one' }, 
+      { title: 'Post Two', body: 'This is post two' }
+  ];
+  function getPosts(){
+    setTimeout(()=>{
+      let output = '';
+      posts.forEach((post, index) => { 
+        output += '<li>$(post.title)</li>';
+      });
+      document.body.innerHTML = output; 
+      }, 1000);
+}
+function createPost(post,callback) { 
+  setTimeout(() => { 
+    posts.push(post); 
+    callback();
+  }, 2000);
+}
+// getPosts();
+
+createPost({title: 'Post three', body: 'This is post three'},getPosts)
+    
+  </script>
+  
+</body>
+</html>
